2de0908659d7fce7ed65085f1909d29a446d4646
Cloé Alcaria <cloe.alcaria@cnes.fr>
2024-05-07 15:08:40 +0200

    Create LoadingSunEphemeris.java

---
+++ src/main/java/ephemeris/LoadingSunEphemeris.java
@@ -0,0 +1,55 @@
+package ephemeris;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import fr.cnes.sirius.addons.patriusdataset.PatriusDataset;
+import fr.cnes.sirius.patrius.bodies.CelestialBody;
+import fr.cnes.sirius.patrius.bodies.CelestialBodyFactory;
+import fr.cnes.sirius.patrius.bodies.JPLEphemeridesLoader;
+import fr.cnes.sirius.patrius.bodies.JPLEphemeridesLoader.EphemerisType;
+import fr.cnes.sirius.patrius.frames.Frame;
+import fr.cnes.sirius.patrius.frames.FramesFactory;
+import fr.cnes.sirius.patrius.orbits.pvcoordinates.PVCoordinates;
+import fr.cnes.sirius.patrius.time.AbsoluteDate;
+import fr.cnes.sirius.patrius.time.TimeScale;
+import fr.cnes.sirius.patrius.time.TimeScalesFactory;
+import fr.cnes.sirius.patrius.utils.exception.PatriusException;
+
+public class LoadingSunEphemeris {
+	 
+    public static void main(final String[] args) throws PatriusException, IOException, URISyntaxException {
+ 
+        // Patrius Dataset initialization
+        PatriusDataset.addResourcesFromPatriusDataset() ;
+ 
+        //String[] fileNames = { "unxp2000.405", "unxp1950.405", "unxp2700.406" };
+        final String[] fileNames = { "unxp.*.405", "unxp.*.406" };
+ 
+        // data for Sun coordinates output
+        final TimeScale tuc = TimeScalesFactory.getUTC();       
+        final AbsoluteDate date = new AbsoluteDate("2010-01-01T12:00:00.000", tuc);
+        final Frame icrf = FramesFactory.getICRF();
+ 
+        for (int i = 0; i < fileNames.length; i++) {
+ 
+            final JPLEphemeridesLoader loader = new JPLEphemeridesLoader(fileNames[i], EphemerisType.SUN);
+ 
+            CelestialBodyFactory.clearCelestialBodyLoaders();
+            CelestialBodyFactory.addCelestialBodyLoader(CelestialBodyFactory.SUN, loader);
+ 
+            // Using the loading theory
+            final CelestialBody sun = loader.loadCelestialBody(CelestialBodyFactory.SUN);
+ 
+            // Coordinates of the Sun at a given date and reference frame
+            final PVCoordinates pv = sun.getPVCoordinates(date, icrf);
+ 
+            System.out.println("");
+            System.out.println(pv.getPosition().getX());
+            System.out.println(pv.getPosition().getY());
+            System.out.println(pv.getPosition().getZ());
+        }
+ 
+    }
+ 
+}
